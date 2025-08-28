ce377028c944cb2a539a1b2561d7df3842b50f97
oleska <oleska1601@gmail.com>
2025-08-28 13:20:39 +0300

    task 5

---
+++ l1/5/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+func writer(stop chan struct{}) chan int64 {
+	ch := make(chan int64)
+	go func() {
+		defer close(ch)
+		var v int64
+		for {
+			select {
+			case <-stop:
+				return
+			case ch <- v:
+				v++
+			}
+		}
+	}()
+	return ch
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("no time was provided")
+	}
+	t, err := strconv.ParseInt(os.Args[1], 10, 64)
+	if err != nil || t <= 0 {
+		log.Fatalln("incorrect time")
+	}
+
+	stopCh := make(chan struct{})
+	ch := writer(stopCh)
+	timeout := time.After(time.Duration(t) * time.Second)
+	for {
+		select {
+		case <-timeout:
+			close(stopCh)
+			return
+		case v, ok := <-ch:
+			if !ok {
+				return
+			}
+			fmt.Println(v)
+		}
+	}
+
+}
