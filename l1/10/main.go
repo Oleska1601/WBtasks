268f37a4c8c543d78b5db32052139f4465f9d5d6
oleska <oleska1601@gmail.com>
2025-08-31 18:14:11 +0300

    task 10

--- l1/10/main.go
+++ l1/10/main.go
@@ -2,12 +2,17 @@ package main
 
 import "fmt"
 
-func main() {
-	lst := []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+func foo(lst []float32) map[int][]float32 {
 	m := make(map[int][]float32)
 	for _, v := range lst {
 		key := int(v/10) * 10
 		m[key] = append(m[key], v)
 	}
+	return m
+}
+
+func main() {
+	lst := []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	m := foo(lst)
 	fmt.Println(m)
 }
